bd5681d6ac909ef638aaf023211b10b830f2583b
vahab <wwwcomir@gmail.com>
2023-08-10 02:30:58 -0700

    add editor.io

--- Chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import index, room
-from django.contrib.auth import views
-urlpatterns = [
-    path('', index, name='index'),
-    path('<str:room_name>/', room, name='room'),
-    path('login', views.LoginView.as_view(template_name='chat/login.html'), name="login")
-]
